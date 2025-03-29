5d8749bae164525a96e28cd2c1883c14148e6461
Akash Karunanayake <akashwishvajith@gmail.com>
2025-03-29 12:49:21 +0530

    Updated gitignore & matadata file

--- src/test/java/backend/BackendApplicationTests.java
+++ src/test/java/backend/BackendApplicationTests.java
@@ -1,12 +1,10 @@
-package com.socialapp.Zircuit;
+package backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class ZircuitApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
