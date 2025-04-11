8dc3f0b9e6f9b7f3f81a3eee13f959ca0d0f8403
gihanikaushalya <gihanikaushalya0@gmail.com>
2025-04-12 06:17:24 +0900

    Created AchievementsController, AchievementsRepository and added codes to both

--- backend/src/main/java/backend/Achievements/repository/AchievementsRepository.java
+++ backend/src/main/java/backend/Achievements/repository/AchievementsRepository.java
@@ -1,5 +1,8 @@
 package backend.Achievements.repository;
 
-public class AchievementsRepository {
+import backend.Achievements.model.AchievementsModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+public interface AchievementsRepository extends MongoRepository<AchievementsModel, String> {
+    void deleteByPostOwnerID(String postOwnerID); // Ensure this method exists
 }
