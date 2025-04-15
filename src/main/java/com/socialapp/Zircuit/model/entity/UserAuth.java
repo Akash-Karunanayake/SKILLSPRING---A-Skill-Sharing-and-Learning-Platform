c1b84f4a711440be9043adf8bb34a66b2928f309
Akash Karunanayake <akashwishvajith@gmail.com>
2025-04-15 19:06:49 +0530

    Updated the entity files of user management

---
+++ src/main/java/com/socialapp/Zircuit/model/entity/UserAuth.java
@@ -0,0 +1,56 @@
+package com.socialapp.Zircuit.model.entity;
+
+import com.socialapp.Zircuit.model.enums.AuthProvider;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_auth")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAuth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auth_id")
+    private Long id;
+
+    @Column(name = "userID", length = 36, nullable = false)
+    private String userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider", nullable = false)
+    private AuthProvider provider;
+
+    @Column(name = "provider_user_id", nullable = false)
+    private String providerUserId;
+
+    @Column(name = "access_token_hash")
+    private String accessTokenHash;
+
+    @Column(name = "refresh_token_hash")
+    private String refreshTokenHash;
+
+    @Column(name = "expiry_time")
+    private LocalDateTime expiryTime;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userID", referencedColumnName = "userID", insertable = false, updatable = false)
+    private User user;
+}
---
+++ src/main/java/com/socialapp/Zircuit/model/entity/UserSettings.java
@@ -0,0 +1,29 @@
+package com.socialapp.Zircuit.model.entity;
+
+import com.socialapp.Zircuit.model.enums.MessagePermission;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_settings")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSettings {
+    @Id
+    @Column(name = "user_id", length = 36)
+    private String userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "allow_messages_from", nullable = false)
+    private MessagePermission allowMessagesFrom = MessagePermission.ALL;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "userID", insertable = false, updatable = false)
+    private User user;
+}
